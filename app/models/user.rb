de84ecbb44418351f0059b1dfd2b497ded93aee4
ItsYou <hman87@outlook.com>
2014-07-18 16:51:51 -0500

    views done in dbc FRiday

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ActiveRecord::Base
+  validates :email, uniqueness: true
+  validates_presence_of :name, :password_hash
+
+  has_many :games
+  has_many :guesses, through: :games
+  has_many :decks, through: :games
+
+  def self.authenticate(args)
+    user = User.find_by_email(args[:email])
+    return user if user && user.password_hash == args[:password]
+    nil
+  end
+
+  def self.create(args)
+    new_user = User.new(args)
+    new_user.password = args[:password]
+    new_user.save!
+    new_user
+  end
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(secret)
+    @password = BCrypt::Password.create(secret)
+    self.password_hash = @password
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+require 'csv'
+def create_states_deck(filename)
+  deck = Deck.create(title: "States/Capitol")
+  CSV.foreach(filename, {:headers => true, :header_converters => :symbol}) do |row|
+    deck.cards << Card.create(row.to_hash)
+  end
+end
+
+create_states_deck("db/capitals.csv")
+
+
+
+
